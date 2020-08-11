0ebc713b0894d396b751192d3c730b9c72077e80
Ramon Audain <raudain123@gmail.com>
2020-08-10 19:14:39 -0700

    Fix javascript expand bug

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-document.querySelectorAll(".project").forEach(button => {
-  button.addEventListener("click", () => {
-    const content = button.lastElementChild; // Declares content id as a variable
-    const toggleClass = button.firstElementChild.children[1].lastElementChild; // Declares icon in nav as variable
+const project = document.querySelectorAll(".project");
+
+project.forEach(button => {
+  button.addEventListener("click", element => {
+    // Declares content id as a variable
+    const content = button.lastElementChild; 
+    // Declares icon in nav as variable
+    const toggleClass = button.firstElementChild.children[1].lastElementChild; 
+    const tagName = element.target.tagName;
     
     // Adds "--active" to the end of project class when clicked
     button.classList.toggle("--active");
 
     // If the class contains "--active", expand target and change the class
-    if (button.classList.contains("--active")) {
+    if (button.classList.contains("--active") && tagName !== "A") {
       content.style.maxHeight = content.scrollHeight + "px";
       content.style.transition = "1s ease-out";
       toggleClass.className = "fas fa-minus";
