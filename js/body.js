d5e1b8180dc1ca4c45b7c04914eea686c47969ba
Ramon Audain <raudain123@gmail.com>
2020-09-26 18:45:49 -0700

    Finish body.js

--- js/body.js
+++ js/body.js
@@ -1,30 +1,17 @@
-let container = document.getElementById("container");
+const body = document.querySelector("body");
+
 const noscript = document.createElement("noscript");
 noscript.textContent = "You need to enable JavaScript to view this site.";
+const contain = document.createElement("div");
+const leftContainer = document.createElement("div");
+const rightContainer = document.createElement("div");
 
-console.log(container);
+// Add ids
+contain.id = "container";
+leftContainer.id = "left-container";rightContainer.id = "right-container";
 
-const btn = document.createElement("button");
-btn.innerHTML = "Test";
-
-function test() {
-    const container = document.createElement("div");
-    const leftContainer = document.createElement("div");
-    const rightContainer = document.createElement("div");
-    
-    container.appendChild(leftContainer);
-    container.appendChild(rightContainer);
-    
-    container.id = "container";
-    leftContainer.id = "left-container";
-    rightContainer.id = "right-container";
-    
-    
-    return test
-}
-
-
-
-container.appendChild(container());
-container.appendChild(noscript);
-container.appendChild(btn);
+// Append
+body.appendChild(noscript);
+body.appendChild(contain);
+contain.appendChild(leftContainer);
+contain.appendChild(rightContainer);
--- js/leftContainer.js
+++ js/leftContainer.js
@@ -1,2 +1,19 @@
-const leftContainer = document.querySelector("left-container");
+const left = document.querySelector("#left-container");
 
+console.log(left)
+
+const headers = document.createElement("div");
+const contact = document.createElement("div");
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h2");
+
+h1.textContent = "RAMON AUDAIN";
+h2.textContent = "Software Developer";
+
+headers.id = "headers";
+contact.id= "contact";
+
+left.appendChild(headers);
+left.appendChild(contact);
+headers.appendChild(h1);
+headers.appendChild(h2);
