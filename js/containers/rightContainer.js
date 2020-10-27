6d2db9801ac67d51fbe6cdb4aa1987cf1a3dde70
Ramon Audain <raudain123@gmail.com>
2020-10-26 20:17:48 -0700

    Format js

--- js/containers/rightContainer.js
+++ js/containers/rightContainer.js
@@ -1,33 +1,33 @@
 const headers = [
-    {
-        header: "SKILLS",
-        id: "skills"
-    },
-    {
-        header: "PROJECTS",
-        id: "projects"
-    },
-    {
-        header: "ABOUT US",
-        id: "about-me"
-    },
+  {
+    header: "SKILLS",
+    id: "skills",
+  },
+  {
+    header: "PROJECTS",
+    id: "projects",
+  },
+  {
+    header: "ABOUT US",
+    id: "about-me",
+  },
 ];
 
 // Find right-container id
 export const right = document.querySelector("#right-container");
 
-headers.forEach(info => {
-    // Create elements
-    const section = document.createElement("section");
-    const h3 = document.createElement("h3");
+headers.forEach((info) => {
+  // Create elements
+  const section = document.createElement("section");
+  const h3 = document.createElement("h3");
 
-    // Add attribute
-    section.id = info.id;
+  // Add attribute
+  section.id = info.id;
 
-    // Append child nodes to parent
-    h3.textContent = info.header;
+  // Append child nodes to parent
+  h3.textContent = info.header;
 
-    // Append child nodes to parent
-    right.appendChild(section);
-    section.appendChild(h3);
+  // Append child nodes to parent
+  right.appendChild(section);
+  section.appendChild(h3);
 });
